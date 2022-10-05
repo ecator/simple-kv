0c34ff7a4c39262b0985e76bdf23bea1401938b1
Martin <qule520@126.com>
2022-10-05 15:16:35 +0800

    feat: 增加powershell编译脚本

---
+++ make.ps1
@@ -0,0 +1,19 @@
+
+$outdir = "release"
+$targetWin="${outdir}\simple-kv.exe"
+$targetLinux="${outdir}\simple-kv"
+
+
+$version = $(git describe --tags --abbrev=0)
+
+$env:GOARCH="amd64"
+
+if (Test-Path $outdir){
+    Remove-Item -Recurse $outdir
+}
+
+$env:GOOS="windows"
+go build -ldflags="-w -s -X 'main.DEBUG=0' -X 'cmd.VERSION=$version'" -o $targetWin
+
+$env:GOOS="linux"
+go build -ldflags="-w -s -X 'main.DEBUG=0' -X 'cmd.VERSION=$version'" -o $targetLinux
